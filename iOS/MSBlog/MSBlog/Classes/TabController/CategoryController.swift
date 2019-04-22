88177a4957be1ef7603338d654aac0183f07e85c
veryitman <veryitman@126.com>
2019-04-22 21:19:24 +0800

    Create tabs.

---
+++ iOS/MSBlog/MSBlog/Classes/TabController/CategoryController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryController.swift
+//  MSBlog
+//
+//  Created by mark.zhang on 2019/4/22.
+//  Copyright © 2019 veryitman. All rights reserved.
+//
+
+import UIKit
+
+class CategoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationItem.title = "Category"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/MSBlog/MSBlog/Classes/TabController/FriendController.swift
@@ -0,0 +1,30 @@
+//
+//  FriendController.swift
+//  MSBlog
+//
+//  Created by mark.zhang on 2019/4/22.
+//  Copyright © 2019 veryitman. All rights reserved.
+//
+
+import UIKit
+
+class FriendController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationItem.title = "Friends"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
