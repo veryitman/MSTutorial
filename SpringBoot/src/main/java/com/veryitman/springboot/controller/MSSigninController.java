f18389538d2a94ac016356c02d682a34526832af
mark.zhang <mark.zhang@dsky.com>
2019-09-14 16:20:05 +0800

    add gitkeep file

--- SpringBoot/src/main/java/com/veryitman/springboot/controller/MSSigninController.java
+++ SpringBoot/src/main/java/com/veryitman/springboot/controller/MSSigninController.java
@@ -19,7 +19,7 @@ public class MSSigninController {
      *
      * http://localhost:8080/signin/name?username=mark&userpwd=123
      */
-    //@CrossOrigin
+//    @CrossOrigin
     @RequestMapping(value = "/name", method = RequestMethod.GET)
     public MSResponse sigin(@RequestParam(value = "username") String userName, @RequestParam(value = "userpwd") String userPwd) {
         MSResponse response = new MSResponse();
--- SpringBoot/src/main/java/com/veryitman/springboot/filter/MSCorsFilter.java
+++ SpringBoot/src/main/java/com/veryitman/springboot/filter/MSCorsFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@Component
+@Component
 public class MSCorsFilter implements Filter {
 
     final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(MSCorsFilter.class);
@@ -20,7 +20,7 @@ public class MSCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Max-Age", "60");
         response.setHeader("Access-Control-Allow-Headers", "*");
         System.out.println("*********************************过滤器被使用**************************");
         chain.doFilter(req, res);
