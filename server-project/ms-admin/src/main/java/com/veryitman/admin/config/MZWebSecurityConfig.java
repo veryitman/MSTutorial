2ad2dc477943375ab46dde8592f7aa65146f9f9b
veryitman <veryitman@126.com>
2020-05-03 11:37:48 +0800

    Admin security

--- server-project/ms-admin/src/main/java/com/veryitman/admin/config/MZWebSecurityConfig.java
+++ server-project/ms-admin/src/main/java/com/veryitman/admin/config/MZWebSecurityConfig.java
@@ -1,19 +1,19 @@
-package com.veryitman.admin.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@EnableWebSecurity
-public class MZWebSecurityConfig implements WebMvcConfigurer {
-
-    @Bean
-    public UserDetailsService userDetailsService() throws Exception {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("admin").roles("administrator").build());
-        return manager;
-    }
-}
+//package com.veryitman.admin.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@EnableWebSecurity
+//public class MZWebSecurityConfig implements WebMvcConfigurer {
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() throws Exception {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("admin").roles("administrator").build());
+//        return manager;
+//    }
+//}
